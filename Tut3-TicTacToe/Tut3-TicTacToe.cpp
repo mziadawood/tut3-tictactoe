68d11a333a661597ba23714d321324f8af874771
mziadawood <mziadawood@gmail.com>
2015-03-15 15:19:06 +0200

    done

--- Tut3-TicTacToe/Tut3-TicTacToe.cpp
+++ Tut3-TicTacToe/Tut3-TicTacToe.cpp
@@ -14,43 +14,65 @@ int main()
 {
 	tic tac;
 
-	cout << "welcome to tictactoe "<<endl;
+	cout << "welcome to tictactoe " << endl;
 	int p = 1;
-
+	int gamestate = 3;
 	for (int i = 0; i < 9; i++)
 	{
-		
-		bool k;
-		int a, b,gamestate ;
+
+		if (gamestate == 3)
+		{
+			bool k;
+		int a, b;
 		char z;
-cout << "please enter co-ordinates of your move player " << p <<" eg 1,1 :"<<endl;
-cin >> a >> z >> b;
+		cout << "please enter co-ordinates of your move player " << p << " eg 1,1 :" << endl;
+		cin >> a >> z >> b;
 
-k = tac.move(a, b, p);
-if (k)
-{
+		k = tac.move(a, b, p);
+		if (k)
+		{
 
-if (p==1)
-{
-	p++;
-	
-}
-else if (p == 2)
-{
-	p--;
-}
+			if (p == 1)
+			{
+				p++;
 
-tac.print();
-gamestate = tac.isWon();
-}
+			}
+			else if (p == 2)
+			{
+				p--;
+			}
 
-cout << gamestate;//still to do
+			tac.print();
+			gamestate = tac.isWon();
+		}
+
+		//gamestate check from here
+
+		if (gamestate == 1)
+		{
+			cout << "player one has achieved success,player two ,,,NOPE";
+
+		}
+		if (gamestate == 2)
+		{
+			cout << "player two has achieved success";
+
+		}
+
 
 
 	}
 
 
+}
+	if (gamestate == 3)
+	{
+		cout << "it has been a draw";
 
-	return 0;
+	}
+
+
+
+	system("pause");
 }
 
